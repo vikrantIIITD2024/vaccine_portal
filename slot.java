66648ed5948ae86a4bbe9add07b2fecd1533d98e
Vikrant Dhanwadia <82530378+vikrantIIITD2024@users.noreply.github.com>
2021-10-11 21:59:48 +0530

    added constructor for slot class

--- slot.java
+++ slot.java
@@ -1,4 +1,14 @@
+import java.math.BigInteger;
 
 public class slot {
-
+	int Day_number;
+	int quantity;
+	
+	
+	public slot(int Day_number, int quantity, int unique_id, BigInteger uniqueID){
+		this.Day_number = Day_number;
+		this.quantity = quantity;
+		
+	}
+	
 }
