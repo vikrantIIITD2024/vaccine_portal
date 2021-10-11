673c914ead7c80d4bc515fa479031f6dd7924fed
Vikrant Dhanwadia <82530378+vikrantIIITD2024@users.noreply.github.com>
2021-10-11 22:02:37 +0530

    code improved

--- hospital.java
+++ hospital.java
@@ -1,13 +1,15 @@
-
 public class hospital {
 	int unique_id;
 	int PinCode;
+	public static int count= 0;
 	String Name;
+	public static String[] name_hospital = new String[50];
+	public static int[] pincode = new int[50];
 	public hospital(String Name, int PinCode){
-		unique_id= (int) (Math.random() * (999999 - 100000 + 1) + 100000);
+		unique_id= 555555+1;
 		this.PinCode= PinCode;
 		this.Name = Name;
+		name_hospital[count] = Name; 
+		pincode[count] = PinCode;		
 	}
-	
-
 }
