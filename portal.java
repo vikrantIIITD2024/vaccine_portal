976539d27637775d4e17f799f5acf9e500a02c4a
Vikrant Dhanwadia <82530378+vikrantIIITD2024@users.noreply.github.com>
2021-10-11 20:40:43 +0530

    changes made(took inputs)

--- portal.java
+++ portal.java
@@ -1,41 +1,96 @@
-import java.util.HashMap;
+
+
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Scanner;
 
 
 
 public class portal {
-	HashMap<Integer,hospital> map = new HashMap<Integer,hospital>();
 	
-	public void register_hospital(String hospi_name, int pode ) {
-		
-		hospital h1 = new hospital(hospi_name, pode );
-		
-		
-		map.put(h1.unique_id,h1);
-
-
-}	
-
-
+	
 
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		int n = s.nextInt();
-		while(n!=8) {
-		int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		
+		
 		System.out.println("CoWin Portal intialized.....");
 		System.out.println("-------------------------------------");
-		System.out.println(arr[0] + ".Add Vaccine");
-		System.out.println(arr[1] + ".Register Hospital");
-		System.out.println(arr[2] + ".Register Citizen");
-		System.out.println(arr[3] + ".Add Slot for Vaccination");
-		System.out.println(arr[4] + ".Book Slot for Vaccination");
-		System.out.println(arr[5] + ".List all slots for a hospital");
-		System.out.println(arr[6] + ".Check Vaccination Status");
-		System.out.println(arr[7] + ".Exit");
+		System.out.println("1.Add Vaccine");
+		System.out.println("2.Register Hospital");
+		System.out.println("3.Register Citizen");
+		System.out.println("4.Add Slot for Vaccination");
+		System.out.println("5.Book Slot for Vaccination");
+		System.out.println("6.List all slots for a hospital");
+		System.out.println("7.Check Vaccination Status");
+		System.out.println("8.Exit");
 		System.out.println("-------------------------------------");
+		int n = s.nextInt();
+		
+      		if (n==1) {
+			add_vaccine();
 		}
-	  }
-}	
+		if (n==2) {
+			register_hospital();
+		}
+		if (n== 3) {
+			register_citizen();
+		}
+		
+		}
+	public static void register_citizen() {
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Citizen Name: ");
+		String name = s.next();
+		System.out.println("Age: ");
+		int age1 = s.nextInt();
+		System.out.println("UniqueID:");
+		BigInteger uniqueID1 = s.nextBigInteger();
+		System.out.println("Citizen Name: " + name + ", Age: " + age1 + ", Unique ID: " + uniqueID1);
+		if (age1 < 18) {
+			System.out.println("Only above 18 are allowed");
+			System.out.println("------------------------------------");
+			main(null);
+
+		} else {
+			System.out.println("------------------------------------");
+			citizen u_i = new citizen(name,age1,uniqueID1);
+			main(null);
+		}
+	}
+	public static void register_hospital() {
+		
+		Scanner s = new Scanner(System.in);
+		System.out.println("Hospital Name: ");
+		String hospi_name = s.next();
+		System.out.println("PinCode: ");
+		int pc= s.nextInt();
+		hospital h1 =new hospital(hospi_name, pc);
+		System.out.println("Hospital Name: "+hospi_name+", PinCode: "+pc+", Unique ID: "+ 555555+hospital.count );
+		System.out.println("------------------------------------");
+		main(null);
+	}
+	public static void add_vaccine() {
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Vaccine Name: ");
+		String v_n = s.next();
+		
+		System.out.println("Number of Doses: ");
+		int nofdoses = s.nextInt();
+		
+		System.out.println("Gap between Doses: ");
+		int gapbetweendoses = s.nextInt();
+		
+		vaccine v1 = new vaccine(v_n, nofdoses,gapbetweendoses);
+		System.out.println("Vaccine Name: "+v_n+", Number of Doses: "+nofdoses+", Gap Between Doses: "+ gapbetweendoses);
+		System.out.println("------------------------------------");
+		main(null);
+	}
 	
+
+	
+	  }
+
